9c4c9a0c92da3e1ef72b4570a8593ba1af50a9b9
Leon Deniz REISINGER <lreisinger18@htl-grieskirchen.local>
2020-10-19 15:47:47 +0200

    readSudoko

--- src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
+++ src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -1,6 +1,13 @@
 package net.htlgrieskirchen.pos3.sudoku;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* Please enter here an answer to task four between the tags:
  * <answerTask4>
@@ -8,14 +15,38 @@ import java.io.File;
  * </answerTask4>
  */
 public class SudokuSolver implements ISodukoSolver {
+
     public SudokuSolver() {
         //initialize if necessary
     }
 
     @Override
-    public final int[][] readSudoku(File file) {
-        // implement this method
-        return new int[0][0]; // delete this line!
+    public final int[][] readSudoku(File file){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            Scanner sc = new Scanner(System.in, "Windows-1252");
+            
+            int rows = 9;
+            int columns = 9;
+            
+            int[][] sudokuArray = new int[rows][columns];
+            while((line = br.readLine()) != null)
+            {
+                String [] parts = line.split(";");
+                for(int i = 0; i < rows; i++)
+                {
+                    for(int j = 0; j < columns; j++)
+                    {
+                        System.out.println(parts[j]);
+                    }
+                }
+            }
+            return sudokuArray;
+        } catch (IOException ex) {
+            Logger.getLogger(SudokuSolver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     @Override
@@ -29,7 +60,7 @@ public class SudokuSolver implements ISodukoSolver {
         // implement this method
         return new int[0][0]; // delete this line!
     }
-    
+
     @Override
     public int[][] solveSudokuParallel(int[][] rawSudoku) {
         // implement this method
